ea92fe1fbf598b35485c4725ae35426c8707c6bb
Andreasafent <afentoulidisandreas@gmail.com>
2025-04-15 22:10:28 +0300

    Finished theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CreateTodo from "./components/createTodo";
 import Todo from "./components/todo";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 function App() {
 
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
 
     useEffect(() => {
         localStorage.setItem('theme', theme);
@@ -19,11 +19,12 @@ function App() {
 
     return (
         <div
-            className="h-[100vh] main w-full flex flex-col"
-            style={{ backgroundColor: "hsl(235, 21%, 11%)" }}
+            className="h-[100vh] main w-full flex flex-col bg-[#e4e5f1] dark:bg-[#161722]"
         >
-            <img src="../public/bg-mobile-dark.jpg" className="block xs:hidden h-[200px] w-full object-cover" />
-            <img src="../public/bg-desktop-dark.jpg" className="hidden xs:block h-[200px] w-full object-cover" />
+            <img src="../public/bg-mobile-dark.jpg" className="xs:hidden light:hidden h-[200px] w-full object-cover" />
+            <img src="../public/bg-mobile-light.jpg" className="xs:hidden dark:hidden h-[200px] w-full object-cover" />
+            <img src="../public/bg-desktop-dark.jpg" className="light:hidden xs:block h-[200px] w-full object-cover" />
+            <img src="../public/bg-desktop-light.jpg" className="dark:hidden xs:block h-[200px] w-full object-cover" />
             <div className="flex items-center justify-center w-full">
 
                 <main className="flex flex-col mt-[-180px] px-[30px] gap-5 py-5 w-full md:w-[768px]">
@@ -38,7 +39,11 @@ function App() {
                             }}
                         >
                             {
-                                theme === 'dark' ? <SunIcon className="h-8 w-8 text-white hover:cursor-pointer" /> : <MoonIcon className="h-8 w-8 text-white hover:cursor-pointer" />
+                                theme === 'dark' ? (
+                                    <SunIcon className="h-8 w-8 text-white hover:cursor-pointer" />
+                                ) : (
+                                    <MoonIcon className="h-8 w-8 text-white hover:cursor-pointer" />
+                                )
                             }
                         </button>
                     </div>
@@ -48,33 +53,32 @@ function App() {
 
 
                     <div
-                        className="text-white flex flex-col rounded-md "
-                        style={{ backgroundColor: " hsl(235, 24%, 19%)" }}
+                        className="bg-[#fafafa] text-white flex flex-col rounded-md dark:bg-[#25273c] shadow-xl "
                     >
                         <Todo />
                         <div className="flex items-center py-4 px-5 justify-between">
                             <div className="w-[110px]">
-                                <p className="text-white/30 text-sm">5 items left</p>
+                                <p className="text-black/50 dark:text-white/30 text-sm">5 items left</p>
                             </div>
-                            <div className="hidden md:flex items-center justify-center gap-5 text-white/30 text-sm" style={{ backgroundColor: " hsl(235, 24%, 19%)" }}>
-                                <button className="hover:text-white font-bold hover:text-white hover:cursor-pointer">All</button>
-                                <button className="active:text-white font-bold hover:text-white hover:cursor-pointer">Active</button>
-                                <button className="active:text-white font-bold hover:text-white hover:cursor-pointer">Completed</button>
+                            <div className="hidden md:flex items-center justify-center gap-5 text-black/50 dark:text-white/30 text-sm">
+                                <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">All</button>
+                                <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Active</button>
+                                <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Completed</button>
                             </div>
                             <div className="w-[110px] text-end">
-                                <p className="text-white/30 text-sm">Clear completed</p>
+                                <p className="text-black/50 dark:text-white/30 text-sm">Clear completed</p>
                             </div>
                         </div>
 
                     </div>
 
-                    <div className="md:hidden flex items-center justify-center gap-5 text-white/30 py-4 rounded-md text-md" style={{ backgroundColor: " hsl(235, 24%, 19%)" }}>
-                        <button className="font-bold">All</button>
-                        <button className="font-bold">Active</button>
-                        <button className="font-bold">Completed</button>
+                    <div className="bg-[#fafafa] dark:bg-[#25273c] md:hidden flex items-center justify-center gap-5 text-black/50 dark:text-white/30 py-4 rounded-md text-md">
+                        <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">All</button>
+                        <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Active</button>
+                        <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Completed</button>
                     </div>
 
-                    <div className="text-white/30 mt-5 text-center text-sm">Drag and drop to reorder list</div>
+                    <div className="text-black/50 dark:text-white/30 mt-5 text-center text-sm">Drag and drop to reorder list</div>
                 </main>
             </div>
         </div>
--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -2,30 +2,29 @@
 // import { useState } from "react";
 
 function CreateTodo() {
-	
 
-	return (
-		<div
-			className="flex items-center py-4 px-5 rounded-md gap-3 mt-2"
-			style={{ backgroundColor: " hsl(235, 24%, 19%)" }}
-		>
-			<input
-				type="checkbox"
-				className="appearance-none icon text-white rounded-full w-5 h-5 border-1 border-gray-700 checked:bg-purple-500"
-			/>
-			<input
-				// value={todo}
-				// onChange={onChange}
-				className="text-white/70 text-xs appearance-none w-full bg-transparent focus:outline-none"
-				placeholder="Create a new todo..."
-				// onKeyDown={(e) => {
-				// 	if (e.key === "Enter") {
-				// 		onAdd()
-				// 	}
-				// }}
-			/>
-		</div>
-	);
+
+    return (
+        <div
+            className="flex items-center py-4 px-5 rounded-md gap-3 mt-2 bg-[#fafafa] dark:bg-[#25273c]"
+        >
+            <input
+                type="checkbox"
+                className="appearance-none icon text-white rounded-full w-5 h-5 border-1 border-gray-700 checked:bg-purple-500"
+            />
+            <input
+                // value={todo}
+                // onChange={onChange}
+                className="text-black dark:text-white text-xs appearance-none w-full bg-transparent focus:outline-none"
+                placeholder="Create a new todo..."
+            // onKeyDown={(e) => {
+            // 	if (e.key === "Enter") {
+            // 		onAdd()
+            // 	}
+            // }}
+            />
+        </div>
+    );
 }
 
 export default CreateTodo;
