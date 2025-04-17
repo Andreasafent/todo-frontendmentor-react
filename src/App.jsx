526e2c6e31dd5ffd6cc099bb80c05e2e9ff40291
Andreasafent <afentoulidisandreas@gmail.com>
2025-04-17 10:39:38 +0300

    Added deleteCompleted

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,10 @@ function App() {
         )
     }
 
+    const deleteCompleted = ()=>{
+        setTodos(todos.filter(todo=> !todo.completed))
+    }
+
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos))
         const count = todos.filter(todo=> !todo.completed).length;
@@ -106,13 +110,13 @@ function App() {
                                         <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Completed</button>
                                     </div>
                                     <div className="w-[110px] text-end">
-                                        <p className="text-black/50 dark:text-white/30 text-sm">Clear completed</p>
+                                        <button onClick={()=> deleteCompleted()} className="text-black/50 dark:text-white/30 text-sm hover:cursor-pointer">Clear completed</button>
                                     </div>
                                 </div>
 
                             </div>
 
-                            <div className="bg-[#fafafa] dark:bg-[#25273c] md:hidden flex items-center justify-center gap-5 text-black/50 dark:text-white/30 py-4 rounded-md text-md">
+                            <div className="bg-[#fafafa] dark:bg-[#25273c] md:hidden flex items-center justify-center gap-5 text-black/50 dark:text-white/30 py-4 rounded-md text-md shadow-lg">
                                 <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">All</button>
                                 <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Active</button>
                                 <button className="hover:text-[#3a7bfd] font-bold hover:cursor-pointer">Completed</button>
